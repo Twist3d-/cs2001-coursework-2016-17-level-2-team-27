83416d109aaad26215367a0067ee9035e9139906
Thomas Hunt <thomas.d.hunt@hotmail.co.uk>
2017-02-24 18:20:34 +0000

    log

---
+++ app/src/main/java/com/turnup/turnup/popup.java
@@ -0,0 +1,20 @@
+package com.turnup.turnup;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class popup extends Activity {
+    @Override
+    //Basic popup screen used after multiple unsuccessful login attempts
+    public void onBackPressed(){}
+    static com.turnup.turnup.popup popup;
+    protected void onCreate(Bundle savedInstanceState) {
+        popup = this;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lockpopup);
+    }
+
+    public static com.turnup.turnup.popup getInstance(){
+        return popup;
+    }
+
+}
